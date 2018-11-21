46af52209835d9cfdf88c89b74dc17a187bd081e
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-11-21 16:21:11 +0100

    Fix transport.rs

--- core/network-libp2p/src/transport.rs
+++ core/network-libp2p/src/transport.rs
@@ -14,8 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
+use futures::prelude::*;
 use libp2p::{self, PeerId, Transport, mplex, secio, yamux};
 use libp2p::core::{either, upgrade, transport::boxed::Boxed, muxing::StreamMuxerBox};
+use libp2p::core::upgrade::{InboundUpgradeExt, OutboundUpgradeExt};
 use libp2p::transport_timeout::TransportTimeout;
 use std::time::Duration;
 use std::usize;
@@ -31,13 +33,21 @@ pub fn build_transport(
 	let base = libp2p::CommonTransport::new()
 		.with_upgrade(secio::SecioConfig::new(local_private_key))
 		.and_then(move |out, endpoint| {
-			let upgrade = upgrade::or(
-				upgrade::map(yamux::Config::default(), either::EitherOutput::First),
-				upgrade::map(mplex_config, either::EitherOutput::Second),
-			);
+			let yamux = yamux::Config::default()
+				.map_inbound(either::EitherOutput::First)
+				.map_outbound(either::EitherOutput::First);
+			let mplex = mplex_config
+				.map_inbound(either::EitherOutput::Second)
+				.map_outbound(either::EitherOutput::Second);
+			let upgrade = yamux
+				.or_inbound(mplex.clone())
+				.or_outbound(mplex.clone());
 			let peer_id = out.remote_key.into_peer_id();
-			let upgrade = upgrade::map(upgrade, move |muxer| (peer_id, muxer));
-			upgrade::apply(out.stream, upgrade, endpoint.into())
+			let peer_id2 = peer_id.clone();
+			let upgrade = upgrade
+				.map_inbound(move |muxer| (peer_id, muxer))
+				.map_outbound(move |muxer| (peer_id2, muxer));
+			upgrade::apply(out.stream, upgrade, endpoint.into()).map_err(|e| e.into_io_error())
 		})
 		.map(|(id, muxer), _| (id, StreamMuxerBox::new(muxer)));
 
